6a5a9345f264b9294cca732044279db7b5afc7d0
Aparna <aparna61295@gmail.com>
2020-08-28 11:08:12 +0530

    Add:Check attendance of an employee for a day

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -1,4 +1,21 @@
 #!/bin/bash -x
 
-echo "----------Welcome to Employee Wage Computation------------"
+echo "----------Welcome to Employee Wage Computation------------";
+
+function checkAttendance()
+{
+
+	isPresent=$((RANDOM%2));
+
+	if [[ $isPresent -eq 1 ]]
+	then
+		echo "Employee is Presnt";
+	else
+		echo "Employee is Absent";
+	fi
+
+}
+
+checkAttendance;
+
 
