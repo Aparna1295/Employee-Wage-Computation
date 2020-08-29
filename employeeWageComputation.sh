9b3b3cd8106469210dff64e90d0841eaad0e2e24
Aparna <aparna61295@gmail.com>
2020-08-29 13:33:28 +0530

    Add:Calculate Monthly Wage of an Employee until 20 working days reaches or 100 working hours reaches

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -12,16 +12,19 @@ function checkAttendance()
 {
 	for i in {1..20}
 		do
-			isPresent=$((RANDOM%2));
-			if [[ $isPresent -eq 1 ]]
-			then
-				echo "Employee is Presnt for Day $i";
-				countP=$(( countP + 1 ));
-				calDailyHours;
-			else
-				echo "Employee is Absent for Day $i";
-         	countA=$(( countA + 1 ));
-			fi
+	   	while [[ $totalHours -lt 100 ]]
+			do
+				isPresent=$((RANDOM%2));
+				if [[ $isPresent -eq 1 ]]
+				then
+					echo "Employee is Presnt for Day $i";
+					countP=$(( countP + 1 ));
+					calDailyHours;
+				else
+					echo "Employee is Absent for Day $i";
+         		countA=$(( countA + 1 ));
+				fi
+			done
 		done
 }
 
@@ -33,34 +36,39 @@ WAGE_PER_HOUR=20;
 EMPLOYEE_HOUR_FULLTIME=8;
 EMPLOYEE_HOUR_PARTTIME=4;
 
+#dailyWage=0;
+
 function calDailyHours(){
-	randomShiftCheck=$((RANDOM%2));
-	case $randomShiftCheck in
-		$IS_FULL_TIME )
-			employeeHour=$((EMPLOYEE_HOUR_FULLTIME));
-	   	echo "Employee is Full Time working for $employeeHour hours";
-			countF=$((countF + 1 ));
-			calDailyWage;
-			;;
-		$IS_PART_TIME )
-			employeeHour=$((EMPLOYEE_HOUR_PARTTIME));
-   		echo "Employee is Part Time working for $employeeHour hours";
-			countH=$((countH + 1 ));
-			calDailyWage;
-			;;
-		* )
-			employeeHour=0;
-   		echo "Employee is not working for the day";
-			;;
-	esac
+		randomShiftCheck=$((RANDOM%2));
+		case $randomShiftCheck in
+			$IS_FULL_TIME )
+				employeeHour=$((EMPLOYEE_HOUR_FULLTIME));
+	   		echo "Employee is Full Time working for $employeeHour hours";
+				countF=$((countF + 1 ));
+				totalHours=$(($totalHours + $employeeHour));
+				calDailyWage;
+				;;
+			$IS_PART_TIME )
+				employeeHour=$((EMPLOYEE_HOUR_PARTTIME));
+   			echo "Employee is Part Time working for $employeeHour hours";
+				countH=$((countH + 1 ));
+				totalHours=$(($totalHours + $employeeHour));
+				calDailyWage;
+				;;
+			* )
+				employeeHour=0;
+   			echo "Employee is not working for the day";
+				;;
+		esac
+
 
 }
 
-function calDailyWage()
-{
+function calDailyWage(){
+
 	dailyWage=$(($employeeHour * $WAGE_PER_HOUR));
 	echo "Daily Wage of an Employee:$dailyWage";
-	salary=$(($salary+$dailyWage));
+	salary=$(($salary + $dailyWage));
 
 }
 
@@ -68,9 +76,10 @@ function calDailyWage()
 function calMonthlyWage(){
 
 	echo "Employee is working for $countP days where $countF FullDays and $countH HalfDays";
-	echo "Monthly Salary of an Employee for $countP: "$salary;
+	echo "Monthly Salary of an Employee for $countP Days: $salary ";
 
 }
 
 checkAttendance;
 calMonthlyWage;
+
